2b875fa861a49bc09f1d81af70abff53412a5ebe
riccardopassi87-bit <riccardo.passi87@gmail.com>
2026-01-27 16:19:42 +0100

    commi

--- backEnd/pulce-backend/src/main/java/com/pulce/pulcebackend/service/NotificationScheduler.java
+++ backEnd/pulce-backend/src/main/java/com/pulce/pulcebackend/service/NotificationScheduler.java
@@ -3,7 +3,9 @@ package com.pulce.pulcebackend.service;
 import com.pulce.pulcebackend.entity.Item;
 import com.pulce.pulcebackend.repository.ItemRepository;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,6 +15,12 @@ import java.util.List;
 
 @Service
 public class NotificationScheduler {
+
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
+    @Value("${app.notification.recipient}")
+    private String adminEmail;
 
     @Autowired
     private ItemRepository itemRepository;
@@ -39,8 +47,8 @@ public class NotificationScheduler {
             String expiration = item.getExpirationDate().toString();
 
             SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo();
-            message.setFrom();
+            message.setTo(adminEmail);
+            message.setFrom(senderEmail);
             message.setSubject(product + ": is going to expire in " + days + " days");
             message.setText(product +"\nis going to expire in " + days + " days! \non: " + expiration);
 
